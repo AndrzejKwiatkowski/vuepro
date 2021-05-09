faa67003cd6305f7fecfd7bbb2c0adde0a04bbea
AndrzejKwiatkowski <34378019+AndrzejKwiatkowski@users.noreply.github.com>
2021-05-09 15:36:16 +0200

    add todo

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,50 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vue-search 
+    :img-photo="'path-img'" 
+    @newitem="newitem()" 
+    @itemselected="itemselected($event)" 
+    :source-field="'name'" 
+    :show-new-botton="true"
+    :search-by-field="false"
+    :api-source="'https://jsonplaceholder.typicode.com/users'"
+    ></vue-search>
+    <div v-for="(arr, index) in arrs" v-bind:key="index">
+      {{ arr.name }}
+    </div>
+    <input type="text" :value='JSON.stringify(arrs)'>
+     <todo-list list-name="My to-dos" />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList.vue'
+import VueSearch from 'vue-input-search/dist/vue-search.common'
 
 export default {
   name: 'App',
+  data() {
+    return { 
+      arrs: []
+     }
+  },
   components: {
-    HelloWorld
+    HelloWorld,
+    VueSearch,
+    TodoList
+  },
+  methods: {
+    newitem: function(){
+      alert('sdsdsd')
+    },
+    itemselected: function(e){
+      this.arrs.push({
+        id: e.name, 
+        name: e.id})
+      console.log(this.arrs)
+    }
   }
 }
 </script>
